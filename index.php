fbcda248cd425e19d7ce9bbbf82e763391292a26
Ankur Khemani <ankurkhemani139@gmail.com>
2016-05-01 07:09:38 -0700

    debugging

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 <head>
 	<title> ChatWise </title>
+	<META http-equiv="refresh" content="0;URL=https://chatwise.azurewebsite.com/php/login.php">
+
 </head>
 
 <body>
 	Chatwise - Know what you are talking!
 	<br>
-	<a href="php/login.php">Try Out Now</a>
-</body>	
+<!-- 	<a href="php/login.php">Try Out Now</a>
+ --></body>	
 
 </html>
