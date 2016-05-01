5a6a72240600791e91dacc9499839bf54cb8054f
Ankur Khemani <ankurkhemani139@gmail.com>
2016-05-01 06:01:43 -0700

    debugging

--- php/create_users.php
+++ php/create_users.php
@@ -42,7 +42,8 @@ include_once "config.inc.php";
 				    	//Post data to setup/initialize user
 					    $data_string = "client_id=".$CLIENT_ID."&client_secret=".$CLIENT_SECRET."&grant_type=".$USER_INITIALIZE_GRANT_TYPE."&uniqueid=".$uniqueid."&timestamp=".$timestamp."&firstname=".$firstname."&lastname=".$lastname."&pictureurl=".$pictureurl;
 					    $uri = $OAUTH_ENDPOINT_DOMAIN."/oauth/token";
-					    echo "uri: " . $uri;
+					    echo "uri: " . $uri . "\n";
+					    echo "data: " . $data_string . "\n"; 
 					    $ch = curl_init();
 					    curl_setopt($ch, CURLOPT_URL,$uri);
 					    curl_setopt($ch, CURLOPT_POST, 1);
@@ -52,8 +53,7 @@ include_once "config.inc.php";
 					    echo "result: ".$result;
 					    $result = json_decode($result, true);
 					    //Get Access Token on Successful Setup & Initialization of the User
-					    //$access_token = $result['access_token'];
-					    $access_token = "osQzMgAAAVRsVIfaAACowFV0OE9wY3VsbW5uM0FQVXd6cUFRa1NLIAAAAANUM1NydjlubVRNNEFtSUc1dHhVbjlnMWtTSEQ5aFNRSWNr";
+					    $access_token = $result['access_token'];
 					    if ($access_token != "")
 					    {
 						    echo "<li class='list-group-item'>".$firstname." ".$lastname." <span style='color:green; float:right;'>(Create User Success)</span></li>";
